77fb6dda07043ec05a031b25bf8a936b271b7b43
Dhaivat Bhatt <dhaivat1994@gmail.com>
2019-10-25 19:36:17 -0400

    train and test faster rcnn with detectron

--- experiments/test_kitti_detectron.py
+++ experiments/test_kitti_detectron.py
@@ -24,8 +24,8 @@ def get_kitti_dicts(root_dir, data_label):
     if data_label == 'test':
         image_names = glob.glob(root_dir+"/images/testing/*.png")
     # print(image_names)
-    # image_names = image_names[0:10]
-    
+    image_names = image_names[0:20]
+    print("total images are:", image_names)
     record = {}
     dataset_dicts = []
 
@@ -35,7 +35,8 @@ def get_kitti_dicts(root_dir, data_label):
     for idx,class_val in enumerate(class_list):
         class_label[class_val] = idx
 
-    for name in image_names:
+    for idx, name in enumerate(image_names):
+        print(idx, name)
         # print(name)
         record = {}
         height, width = cv2.imread(name).shape[:2]
@@ -89,27 +90,6 @@ for d in ["train", "test"]:
 kitti_metadata = MetadataCatalog.get('kitti/train')
 
 
-print("data loading")
-# dataset_dicts = get_kitti_dicts(root_dir, 'train')
-# print(dataset_dicts[0]['annotations'])
-
-# for d in random.sample(dataset_dicts, 15):
-#     # print(d)
-#     # print(d["file_name"])
-#     img = cv2.imread(d["file_name"])
-#     visualizer = Visualizer(img[:, :, ::-1], metadata=kitti_metadata, scale=1.0)
-#     vis = visualizer.draw_dataset_dict(d)
-    # print(vis.get_image()[:, :, ::-1].shape)   
-    # while(1):
-    #     cv2.imshow('', vis.get_image()[:, :, ::-1])
-    #     k = cv2.waitKey(33)
-    #     if k == -1:
-    #         continue
-    #     elif k == 27:
-    #         break
-
-
-
 """Now, let's fine-tune a coco-pretrained R50-FPN Mask R-CNN model on the balloon dataset. It takes ~6 minutes to train 300 iterations on Colab's K80 GPU."""
 
 from detectron2.engine import DefaultTrainer
@@ -117,45 +97,41 @@ from detectron2.config import get_cfg
 
 cfg = get_cfg()
 cfg.merge_from_file("/network/home/bhattdha/detectron2/configs/COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
-cfg.DATASETS.TRAIN = ("kitti/train",)
-cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
-cfg.DATALOADER.NUM_WORKERS = 2
+# cfg.DATASETS.TRAIN = ("kitti/train",)
+# cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
 cfg.MODEL.WEIGHTS = "detectron2://COCO-Detection/faster_rcnn_R_101_FPN_3x/137851257/model_final_f6e8b1.pkl"  # initialize from model zoo
-cfg.SOLVER.IMS_PER_BATCH = 12
-cfg.SOLVER.BASE_LR = 0.015  
-cfg.SOLVER.MAX_ITER =  40000   # 300 iterations seems good enough, but you can certainly train longer
+# cfg.SOLVER.IMS_PER_BATCH = 12
+# cfg.SOLVER.BASE_LR = 0.015  
+# cfg.SOLVER.MAX_ITER =  40000   # 300 iterations seems good enough, but you can certainly train longer
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(class_list)  #  (kitti)
 cfg.OUTPUT_DIR = '/network/tmp1/bhattdha/detectron2_kitti/'
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg) 
-trainer.resume_or_load(resume=True)
-print("start training")
-trainer.train()
 
 
-"""Now, we perform inference with the trained model on the balloon validation dataset. First, let's create a predictor using the model we just trained:"""
-
-cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+"""Now, we perform inference with the trained model on the kitti dataset. First, let's create a predictor using the model we just trained:"""
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_0014999.pth")
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set the testing threshold for this model
-cfg.DATASETS.TEST = ("kitti/test", )
+# cfg.DATASETS.TEST = ("kitti/test", )
 predictor = DefaultPredictor(cfg)
 
 """Then, we randomly select several samples to visualize the prediction results."""
 
 from detectron2.utils.visualizer import ColorMode
-dataset_dicts = get_kitti_dicts("/network/tmp1/bhattdha/kitti_dataset", 'test')
-for d in random.sample(dataset_dicts, 3):    
-    im = cv2.imread(d["file_name"])
-    print(im)
+# dataset_dicts = get_kitti_dicts("/network/tmp1/bhattdha/kitti_dataset", 'test')
+image_names = glob.glob(root_dir+"/images/testing/*.png")
+for idx, im_name in enumerate(image_names):   
+    print(idx, im_name)
+    im = cv2.imread(im_name)
     outputs = predictor(im)
     v = Visualizer(im[:, :, ::-1],
                    metadata=kitti_metadata, 
                    scale=1.0, 
-                   instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels
+                   instance_mode=ColorMode.IMAGE   
     )
 
     v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    
+    print("saving images")
+    print(type(v))
+    cv2.imwrite("/network/tmp1/bhattdha/detectron2_kitti/test_outputs/" + str(idx).zfill(5) + '.png', v.get_image()[:, :, ::-1]) 
     # cv2_imshow(v.get_image()[:, :, ::-1])
     
